3767dcc876b496a4adea20434fcfaaaf8c7ebb93
deivit <deivit@mail.com>
2025-10-25 19:17:29 +0200

    Refactored

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='binance',
     version='0.1.0',
-    packages=find_packages(),
+    packages=["binance"],
+    package_dir={"binance", "."},
     install_requires=[
         "utils"
     ],
